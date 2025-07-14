4942c63ca991e88ab91cf19992b5e13282eff2f8
Yevhenii Vlasenko <vlasenkoevy@gmail.com>
2025-07-14 17:32:11 +0300

    repository cleanup

--- src/main/java/com/testomatio/reporter/client/urlbuilder/UrlBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.testomatio.reporter.client.urlbuilder;
-
-public interface UrlBuilder {
-    String buildCreateRunUrl();
-
-    String buildReportTestUrl(String testRunUid);
-
-    String buildFinishTestRunUrl(String testRunUid);
-}
--- src/main/java/com/testomatio/reporter/exception/FinishReportFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.testomatio.reporter.exception;
-
-public class FinishReportFailedException extends RuntimeException {
-    public FinishReportFailedException(String message) {
-        super(message);
-    }
-}
