8b769bef6f45ea25edc47156fd3d98ff1f85f235
Yevhenii Vlasenko <vlasenkoevy@gmail.com>
2025-07-30 18:12:36 +0300

    added testomatio.listening to switch listening (testng);

--- java-reporter-testng/src/main/java/io/testomat/testng/listener/TestNgListener.java
+++ java-reporter-testng/src/main/java/io/testomat/testng/listener/TestNgListener.java
@@ -4,6 +4,8 @@ import static io.testomat.core.constants.CommonConstants.FAILED;
 import static io.testomat.core.constants.CommonConstants.PASSED;
 import static io.testomat.core.constants.CommonConstants.SKIPPED;
 
+import io.testomat.core.propertyconfig.impl.PropertyProviderFactoryImpl;
+import io.testomat.core.propertyconfig.interf.PropertyProvider;
 import io.testomat.core.runmanager.GlobalRunManager;
 import io.testomat.testng.methodexporter.TestNgMethodExportManager;
 import io.testomat.testng.reporter.TestNgTestResultReporter;
@@ -26,34 +28,43 @@ import org.testng.ITestResult;
  */
 public class TestNgListener implements ISuiteListener, ITestListener, IInvokedMethodListener {
     private static final Logger log = LoggerFactory.getLogger(TestNgListener.class);
+    private static final String LISTENING_REQUIRED_PROPERTY_NAME = "testomatio.listening";
 
     private final GlobalRunManager runManager;
     private final TestNgTestResultReporter reporter;
     private final TestNgMethodExportManager methodExportManager;
+    private final PropertyProvider provider;
 
     private final Set<String> processedClasses;
 
     public TestNgListener() {
-        this.runManager = GlobalRunManager.getInstance();
-        this.reporter = new TestNgTestResultReporter();
         this.methodExportManager = new TestNgMethodExportManager();
         this.processedClasses = ConcurrentHashMap.newKeySet();
+        this.runManager = GlobalRunManager.getInstance();
+        this.reporter = new TestNgTestResultReporter();
+        this.provider =
+                PropertyProviderFactoryImpl.getPropertyProviderFactory().getPropertyProvider();
     }
 
     /**
      * Constructor for testing
      */
-    public TestNgListener(GlobalRunManager runManager,
+    public TestNgListener(TestNgMethodExportManager methodExportManager,
                           TestNgTestResultReporter reporter,
-                          TestNgMethodExportManager methodExportManager) {
+                          GlobalRunManager runManager,
+                          PropertyProvider provider) {
         this.runManager = runManager;
         this.reporter = reporter;
         this.methodExportManager = methodExportManager;
+        this.provider = provider;
         this.processedClasses = ConcurrentHashMap.newKeySet();
     }
 
     @Override
     public void onStart(ISuite suite) {
+        if (!isListeningRequired()) {
+            return;
+        }
         log.debug("Suite started: {}", suite.getName());
         runManager.incrementSuiteCounter();
         reporter.reportTestResult(suite);
@@ -61,12 +72,18 @@ public class TestNgListener implements ISuiteListener, ITestListener, IInvokedMe
 
     @Override
     public void onFinish(ISuite suite) {
+        if (!isListeningRequired()) {
+            return;
+        }
         log.debug("Suite finished: {}", suite.getName());
         runManager.decrementSuiteCounter();
     }
 
     @Override
     public void onFinish(ITestContext context) {
+        if (!isListeningRequired()) {
+            return;
+        }
         log.debug("Test context finished: {}", context.getName());
 
         for (Class<?> testClass : context.getAllTestMethods()[0]
@@ -84,18 +101,27 @@ public class TestNgListener implements ISuiteListener, ITestListener, IInvokedMe
 
     @Override
     public void onTestSuccess(ITestResult result) {
+        if (!isListeningRequired()) {
+            return;
+        }
         reporter.reportTestResult(result, PASSED);
         exportTestClassIfNotProcessed(result.getTestClass().getRealClass());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+        if (!isListeningRequired()) {
+            return;
+        }
         reporter.reportTestResult(result, FAILED);
         exportTestClassIfNotProcessed(result.getTestClass().getRealClass());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
+        if (!isListeningRequired()) {
+            return;
+        }
         reporter.reportTestResult(result, SKIPPED);
         exportTestClassIfNotProcessed(result.getTestClass().getRealClass());
     }
@@ -111,6 +137,14 @@ public class TestNgListener implements ISuiteListener, ITestListener, IInvokedMe
             methodExportManager.loadTestBodyForClass(testClass);
         } else {
             log.debug("Test class {} already processed", className);
+        }
+    }
+
+    private boolean isListeningRequired() {
+        try {
+            return provider.getProperty(LISTENING_REQUIRED_PROPERTY_NAME) != null;
+        } catch (Exception e) {
+            return false;
         }
     }
 }
