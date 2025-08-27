07735fff64de119a2c9c5a99a2c4350d32a41803
Yevhenii Vlasenko <vlasenkoevy@gmail.com>
2025-08-27 16:29:03 +0300

    Updated javadoc for junit module

--- java-reporter-junit/src/main/java/io/testomat/junit/exception/MethodExporterException.java
+++ java-reporter-junit/src/main/java/io/testomat/junit/exception/MethodExporterException.java
@@ -1,5 +1,10 @@
 package io.testomat.junit.exception;
 
+/**
+ * Base exception for method export-related failures.
+ * This exception is thrown when test method body extraction, parsing,
+ * or transmission to the Testomat.io API encounters errors.
+ */
 public class MethodExporterException extends RuntimeException {
     public MethodExporterException(String message) {
         super(message);
--- java-reporter-junit/src/main/java/io/testomat/junit/exception/ReporterException.java
+++ java-reporter-junit/src/main/java/io/testomat/junit/exception/ReporterException.java
@@ -1,5 +1,9 @@
 package io.testomat.junit.exception;
 
+/**
+ * Exception thrown when test result reporting to Testomat.io fails.
+ * This is a general-purpose exception for reporting-related failures.
+ */
 public class ReporterException extends RuntimeException {
     public ReporterException(String message, Throwable e) {
         super(message, e);
