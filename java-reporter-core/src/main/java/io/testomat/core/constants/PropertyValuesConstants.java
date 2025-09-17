3c0235d5cab7cf989c6150790690d6ecc906a9f3
Yevhenii Vlasenko <vlasenkoevy@gmail.com>
2025-09-17 19:37:10 +0300

    Changed batch flush time to 10 seconds

--- java-reporter-core/src/main/java/io/testomat/core/constants/PropertyValuesConstants.java
+++ java-reporter-core/src/main/java/io/testomat/core/constants/PropertyValuesConstants.java
@@ -1,7 +1,7 @@
 package io.testomat.core.constants;
 
 public class PropertyValuesConstants {
-    public static final int DEFAULT_BATCH_SIZE = 100000;
+    public static final int DEFAULT_BATCH_SIZE = 10;
     public static final int DEFAULT_FLUSH_INTERVAL_SECONDS = 60000;
     public static final String DEFAULT_URL = "https://app.testomat.io/";
     public static final String DEFAULT_RUN_TITLE = "Default Run Title";
