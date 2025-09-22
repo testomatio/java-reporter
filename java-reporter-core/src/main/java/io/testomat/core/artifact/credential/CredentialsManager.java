1fd0fc6949e99a4b92f70ee03a3272dbc5947184
Yevhenii Vlasenko <vlasenkoevy@gmail.com>
2025-09-22 20:35:12 +0300

    refactored artifact related code

--- java-reporter-core/src/main/java/io/testomat/core/artifact/credential/CredentialsManager.java
+++ java-reporter-core/src/main/java/io/testomat/core/artifact/credential/CredentialsManager.java
@@ -17,10 +17,8 @@ import static io.testomat.core.constants.CredentialConstants.REGION;
 import static io.testomat.core.constants.CredentialConstants.SECRET_ACCESS_KEY;
 import static io.testomat.core.constants.CredentialConstants.SHARED;
 
-import io.testomat.core.exception.ArtifactManagementException;
 import io.testomat.core.propertyconfig.impl.PropertyProviderFactoryImpl;
 import io.testomat.core.propertyconfig.interf.PropertyProvider;
-import java.lang.reflect.Field;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,13 +27,6 @@ public class CredentialsManager {
     private static final Logger log = LoggerFactory.getLogger(CredentialsManager.class);
     private static final S3Credentials credentials = new S3Credentials();
 
-    private static final String SECRET_ACCESS_KEY_FIELD = "secretAccessKey";
-    private static final String ACCESS_KEY_ID_FIELD = "accessKeyId";
-    private static final String BUCKET_FIELD = "bucket";
-    private static final String REGION_FIELD = "region";
-    private static final String PRESIGN_FIELD = "presign";
-    private static final String ENDPOINT_FIELD_NAME = "customEndpoint";
-    private static final String FORCE_PATH_STYLE_FIELD = "forcePath";
 
     private final PropertyProvider provider =
             PropertyProviderFactoryImpl.getPropertyProviderFactory().getPropertyProvider();
@@ -51,65 +42,29 @@ public class CredentialsManager {
             log.warn("Received null or empty credentials map");
             return;
         }
-        if (getPropertyFromEnv(FORCE_PATH_PROPERTY_NAME) != null) {
-            populateCredentialsFromEnv(getPropertyFromEnv(FORCE_PATH_PROPERTY_NAME), FORCE_PATH_STYLE_FIELD);
-            log.debug("ForcePath from env");
-        } else {
-            credentials.setPresign(getBoolean(credsFromServer, FORCE_PATH, false));
-            log.debug("ForcePath from server");
-        }
+        populateCredentialField(FORCE_PATH_PROPERTY_NAME, FORCE_PATH, credsFromServer, "ForcePath",
+                value -> credentials.setForcePath(getBooleanValue(value)));
 
+        populateCredentialField(ENDPOINT_PROPERTY_NAME, ENDPOINT, credsFromServer, "Endpoint",
+                value -> credentials.setCustomEndpoint(getStringValue(value)));
 
-        if (getPropertyFromEnv(ENDPOINT_PROPERTY_NAME) != null) {
-            populateCredentialsFromEnv(getPropertyFromEnv(ENDPOINT_PROPERTY_NAME), ENDPOINT_FIELD_NAME);
-            log.debug("Endpoint from env");
-        } else {
-            credentials.setCustomEndpoint(getString(credsFromServer, ENDPOINT));
-            log.debug("Endpoint from server");
-        }
+        populateCredentialField(PRIVATE_ARTIFACTS_PROPERTY_NAME, PRESIGN, credsFromServer, "Presign",
+                value -> credentials.setPresign(getBooleanValue(value)));
 
-        if (getPropertyFromEnv(PRIVATE_ARTIFACTS_PROPERTY_NAME) != null) {
-            populateCredentialsFromEnv(getPropertyFromEnv(PRIVATE_ARTIFACTS_PROPERTY_NAME), PRESIGN_FIELD);
-            log.debug("Presign from env");
-        } else {
-            credentials.setPresign(getBoolean(credsFromServer, PRESIGN, false));
-            log.debug("Presign from server");
-        }
+        populateCredentialField(SECRET_ACCESS_KEY_PROPERTY_NAME, SECRET_ACCESS_KEY, credsFromServer, "SecretAccessKey",
+                value -> credentials.setSecretAccessKey(getStringValue(value)));
 
-        if (getPropertyFromEnv(SECRET_ACCESS_KEY_PROPERTY_NAME) != null) {
-            populateCredentialsFromEnv(getPropertyFromEnv(SECRET_ACCESS_KEY_PROPERTY_NAME), SECRET_ACCESS_KEY_FIELD);
-            log.debug("SecretAccessKey from env");
-        } else {
-            credentials.setSecretAccessKey(getString(credsFromServer, SECRET_ACCESS_KEY));
-            log.debug("SecretAccessKey from server");
-        }
+        populateCredentialField(ACCESS_KEY_PROPERTY_NAME, ACCESS_KEY_ID, credsFromServer, "AccessKey",
+                value -> credentials.setAccessKeyId(getStringValue(value)));
 
-        if (getPropertyFromEnv(ACCESS_KEY_PROPERTY_NAME) != null) {
-            populateCredentialsFromEnv(getPropertyFromEnv(ACCESS_KEY_PROPERTY_NAME), ACCESS_KEY_ID_FIELD);
-            log.debug("AccessKey from env");
-        } else {
-            credentials.setAccessKeyId(getString(credsFromServer, ACCESS_KEY_ID));
-            log.debug("AccessKey from server");
-        }
+        populateCredentialField(BUCKET_PROPERTY_NAME, BUCKET, credsFromServer, "Bucket",
+                value -> credentials.setBucket(getStringValue(value)));
 
-        if (getPropertyFromEnv(BUCKET_PROPERTY_NAME) != null) {
-            populateCredentialsFromEnv(getPropertyFromEnv(BUCKET_PROPERTY_NAME), BUCKET_FIELD);
-            log.debug("Bucket from env");
-        } else {
-            credentials.setBucket(getString(credsFromServer, BUCKET));
-            log.debug("Bucket from server");
-        }
+        populateCredentialField(REGION_PROPERTY_NAME, REGION, credsFromServer, "Region",
+                value -> credentials.setRegion(getStringValue(value)));
 
-        if (getPropertyFromEnv(REGION_PROPERTY_NAME) != null) {
-            populateCredentialsFromEnv(getPropertyFromEnv(REGION_PROPERTY_NAME), REGION_FIELD);
-            log.debug("Region from env");
-        } else {
-            credentials.setRegion(getString(credsFromServer, REGION));
-            log.debug("Region from server");
-        }
-
-        credentials.setIam(getBoolean(credsFromServer, IAM, false));
-        credentials.setShared(getBoolean(credsFromServer, SHARED, false));
+        credentials.setIam(getBooleanValue(credsFromServer.get(IAM)));
+        credentials.setShared(getBooleanValue(credsFromServer.get(SHARED)));
 
         logCredentialsInitializationResult();
     }
@@ -133,16 +88,6 @@ public class CredentialsManager {
         return allAvailable;
     }
 
-    private boolean getBoolean(Map<String, Object> map, String key, boolean defaultValue) {
-        Object value = map.get(key);
-        return value != null ? Boolean.parseBoolean(value.toString()) : defaultValue;
-    }
-
-    private String getString(Map<String, Object> map, String key) {
-        Object value = map.get(key);
-        return value != null ? value.toString() : null;
-    }
-
     private Object getPropertyFromEnv(String propertyName) {
         try {
             return provider.getProperty(propertyName);
@@ -151,18 +96,28 @@ public class CredentialsManager {
         }
     }
 
-    private void populateCredentialsFromEnv(Object envPropertyValue, String fieldName) {
-        Field field;
-        try {
-            field = credentials.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(credentials, envPropertyValue);
-        } catch (NoSuchFieldException e) {
-            throw new ArtifactManagementException("Failed to set credentials with reflection. "
-                    + "The field does not exist: " + fieldName);
-        } catch (IllegalAccessException e) {
-            throw new ArtifactManagementException("Inaccessible field: " + fieldName);
+    private void populateCredentialField(String envPropertyName, String serverKey,
+                                         Map<String, Object> credsFromServer, String fieldDisplayName,
+                                         java.util.function.Consumer<Object> setter) {
+        Object envValue = getPropertyFromEnv(envPropertyName);
+        if (envValue != null) {
+            setter.accept(envValue);
+            log.debug("{} from env", fieldDisplayName);
+        } else {
+            Object serverValue = credsFromServer.get(serverKey);
+            if (serverValue != null) {
+                setter.accept(serverValue);
+            }
+            log.debug("{} from server", fieldDisplayName);
         }
+    }
+
+    private String getStringValue(Object value) {
+        return value != null ? value.toString() : null;
+    }
+
+    private boolean getBooleanValue(Object value) {
+        return value != null && Boolean.parseBoolean(value.toString());
     }
 
     private void logCredentialsInitializationResult() {
--- java-reporter-core/src/main/java/io/testomat/core/artifact/credential/CredentialsValidationService.java
+++ java-reporter-core/src/main/java/io/testomat/core/artifact/credential/CredentialsValidationService.java
@@ -1,16 +1,23 @@
 package io.testomat.core.artifact.credential;
 
+import io.testomat.core.artifact.client.AwsClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
 public class CredentialsValidationService {
     private static final Logger log = LoggerFactory.getLogger(CredentialsValidationService.class);
+    private final AwsClient awsClient;
+
+    public CredentialsValidationService() {
+        this.awsClient = new AwsClient();
+    }
+
+    public CredentialsValidationService(AwsClient awsClient) {
+        this.awsClient = awsClient;
+    }
 
     public boolean areCredentialsValid(S3Credentials creds) {
         if (creds == null) {
@@ -30,12 +37,8 @@ public class CredentialsValidationService {
         log.debug("Validating S3 credentials for bucket: {} in region: {}",
                 creds.getBucket(), creds.getRegion());
 
-        try (S3Client client = S3Client.builder()
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(creds.getAccessKeyId(), creds.getSecretAccessKey())))
-                .region(Region.of(creds.getRegion()))
-                .build()) {
-
+        try {
+            S3Client client = awsClient.getS3Client();
             HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
                     .bucket(creds.getBucket())
                     .build();
