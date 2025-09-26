ef998cc5e400d79cbface41be10da3c1155a1c18
Yevhenii Vlasenko <vlasenkoevy@gmail.com>
2025-09-26 13:30:52 +0300

    Added the delay before artifact batch sending

--- java-reporter-core/src/main/java/io/testomat/core/runmanager/GlobalRunManager.java
+++ java-reporter-core/src/main/java/io/testomat/core/runmanager/GlobalRunManager.java
@@ -255,6 +255,7 @@ public class GlobalRunManager {
                 log.debug("Test run finished: {} (duration: {}s)", uid, duration);
 
                 ReportedTestStorage.linkArtifactsToTests(ArtifactLinkDataStorage.ARTEFACT_LINK_DATA_STORAGE);
+                Thread.sleep(15000);
                 client.sendTestWithArtifacts(uid);
                 log.debug("Artifacts sent successfully for run: {}", uid);
             } catch (IOException e) {
